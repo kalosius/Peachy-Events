69226e797af2384be03e668dce2f3583d61119af
kalosius <kalosius43@gmail.com>
2024-07-10 19:04:36 +0300

    Added search bar

--- events/views.py
+++ events/views.py
@@ -4,7 +4,21 @@ from calendar import HTMLCalendar
 from datetime import datetime
 from . models import Event, Venue
 from .forms import VenueForm
+
 from django.http import HttpResponseRedirect
+
+def search_venues(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        venues = Venue.objects.filter(name__contains=searched)
+        return render(request, 'events/search_venues.html', {'searched':searched, 'venues':venues})
+    else:
+        return render(request, 'events/search_venues.html', {})
+
+
+
+
+
 
 def show_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
