d11860fe1daf07f5b09c26006b3ef77553eb8f4b
kalosius <kalosius43@gmail.com>
2024-07-11 15:02:41 +0300

    Update Venue

--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
@@ -6,6 +6,17 @@ from . models import Event, Venue
 from .forms import VenueForm
 
 from django.http import HttpResponseRedirect
+
+def update_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    form = VenueForm(request.POST or None, instance=venue)
+    if form.is_valid():
+        form.save()
+        return redirect('list-venues')
+    return render(request, 'events/update_venue.html', {'venue':venue, 'form':form})
+
+
+
 
 def search_venues(request):
     if request.method == "POST":
