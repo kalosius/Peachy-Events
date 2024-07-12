3bb3b88bd92779bccdbf77f685caf139b901a0f1
kalosius <kalosius43@gmail.com>
2024-07-12 22:36:42 +0300

    admin modified

--- myclub/urls.py
+++ myclub/urls.py
@@ -6,3 +6,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
 ]
+
+# Configure admin titles
+admin.site.site_header = "Peachy Boys Administration"
+admin.site.site_title = "Peachy Boys Administration"
+admin.site.index_title = "Welcome To The Peachy Administration Area"
+
+
