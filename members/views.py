e6b1df1dca318e986d1d0479fa00db0b8122fdbd
kalosius <kalosius43@gmail.com>
2024-07-14 22:16:45 +0300

    register form created

--- members/views.py
+++ members/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Registration Successful")
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {'form': form})
 
 def logout_user(request):
     logout(request)
