6f95ba7b2f197eac2e3a54c7e92c9c630471cf09
kalosius <kalosius43@gmail.com>
2024-07-15 12:51:03 +0300

    bootstrapfied registeruser form

--- members/views.py
+++ members/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -16,7 +17,7 @@ def register_user(request):
             messages.success(request, "Registration Successful")
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
 
     return render(request, 'authenticate/register_user.html', {'form': form})
 
