00cc98ce2eafef20bfbaf0ec2db036b6f1e37cce
kalosius <kalosius43@gmail.com>
2024-07-16 13:00:40 +0300

    venue owner

--- events/views.py
+++ events/views.py
@@ -3,6 +3,7 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 from . models import Event, Venue
+from django.contrib.auth.models import User
 from .forms import VenueForm, EventForm, EventFormAdmin
 from django.contrib import messages
 from django.http import HttpResponseRedirect
@@ -165,7 +166,8 @@ def search_venues(request):
 
 def show_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
-    return render(request, 'events/show_venue.html', {'venue':venue})
+    venue_owner = User.objects.get(pk=venue.owner)
+    return render(request, 'events/show_venue.html', {'venue':venue, 'venue_owner':venue_owner})
 
 
 def list_venues(request):
